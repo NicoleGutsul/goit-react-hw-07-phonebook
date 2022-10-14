4a443cc2c3567eb4af48a6f6f67bca5f0416f758
Nicole Gutsul <ijustnicole@gmail.com>
2022-10-14 12:32:22 +0100

    creates operations logic

---
+++ src/redux/operations.js
@@ -0,0 +1,31 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://634874330484786c6e9a0abd.mockapi.io/";
+
+export const fetchNumbers = createAsyncThunk('contacts/fetchAll', async (_, thunkAPI) => {
+    try {
+        const res = await axios.get('/contacts')
+        return res.data
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message)
+    }
+});
+
+export const addContact = createAsyncThunk('contacts/addContact', async (newContact, thunkAPI) => {
+    try {
+        const res = await axios.post('/contacts', newContact)
+        return res.data 
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message)
+    }
+});
+
+export const deleteContact = createAsyncThunk('contacts/deleteContact', async (contactId, thunkAPI) => {
+    try {
+        const res = await axios.delete(`/contacts/${contactId}`)
+        return res.data 
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message)
+    }
+});
